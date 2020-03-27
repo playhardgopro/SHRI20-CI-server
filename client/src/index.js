8ffd42d7f77a74a115af41c143179017010cde4e
Daniel <grandilewski@gmail.com>
2020-03-27 22:51:19 +0300

    Add: redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,41 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { combineReducers, createStore } from 'redux'
+
 import App from './App'
-import store from './app/store'
+
+const initialState = {
+  name: 'foo',
+  attrs: 'bar',
+}
+
+function test(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_TEST':
+      return { ...state, name: action.name }
+    default:
+      return state
+  }
+}
+
+const reducers = combineReducers({ test })
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+function d() {
+  store.dispatch({
+    type: 'SET_TEST',
+    name: 'test',
+  })
+}
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      {d()}
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
