f3276aa0f7f857fd8747e461ae549cc016a2f0f7
Daniel <grandilewski@gmail.com>
2020-04-05 18:28:17 +0300

    Refactor: Home

--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Footer, Header, Layout, RepConnection, Grid } from '../../index'
+import { Footer, Header, Layout, RepConnection, Grid } from '../..'
 
 const grid = {
   block: {
