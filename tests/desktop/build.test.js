f85ec3b29395896631e7668a7694990d94164180
Daniel <grandilewski@gmail.com>
2020-04-14 21:56:21 +0300

    Add: npm commands

--- tests/desktop/build.test.js
+++ tests/desktop/build.test.js
@@ -1,14 +1,6 @@
 const { assert } = require('chai')
 
 describe('Build', function () {
-  it('list exists', function () {
-    return this.browser
-      .url('http://localhost:3000/build/1')
-      .isExisting('.card')
-      .then((exists) => {
-        assert.ok(exists, 'Карточка билда не появилась')
-      })
-  })
   it('screenshot test', async function () {
     return this.browser.url('http://localhost:3000/build/1').assertView('build', 'body', {
       allowViewportOverflow: true,
