200924b6f11a62565ee342ac29f253171d77b24b
Daniel <grandilewski@gmail.com>
2020-04-24 23:38:56 +0300

    Client: ts config

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,30 @@
 /// <reference types="react-scripts" />
+declare type ResponseSuccess = 'success'
+
+declare interface BuildSettings {
+  isCached?: boolean
+  repoName: string
+  buildCommand: string
+  mainBranch: string
+  period: number
+}
+
+declare interface CommitInfo {
+  branchName: string
+  authorName: string
+  commitHash: string
+  commitMessage: string
+}
+
+declare interface BuildTask {
+  id: string
+  configurationId: string
+  buildNumber: number
+  commitMessage: string
+  commitHash: string
+  branchName: string
+  authorName: string
+  status: string
+  start: string
+  duration: number
+}
