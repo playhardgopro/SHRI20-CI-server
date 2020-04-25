9164cbe09e9496f5aa56877507f92e6f6a0a0f97
Daniel <grandilewski@gmail.com>
2020-04-25 03:45:13 +0300

    Client: text + ts

--- client/src/components/UI/Text/Text.tsx
+++ client/src/components/UI/Text/Text.tsx
@@ -1,23 +1,16 @@
 import * as React from 'react'
-import PropTypes from 'prop-types'
 import { withNaming } from '@bem-react/classname'
 import './Text.scss'
 
 const cn = withNaming({ n: '', e: '__', m: '_' })
+interface TextProps {
+  className: {}
+}
 
-const Text = ({ children, className }) => {
+const Text: React.FC<TextProps> = ({ children, className }) => {
   const cnText = cn('text')
 
   return <span className={cnText(className)}>{children}</span>
-}
-
-Text.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  className: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.bool])),
-}
-Text.defaultProps = {
-  children: '',
-  className: {},
 }
 
 export default Text
