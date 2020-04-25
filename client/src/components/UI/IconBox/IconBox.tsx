b902a2896daee985a80a89a08d70e8de5ef480d2
Daniel <grandilewski@gmail.com>
2020-04-25 03:11:01 +0300

    Client: iconbox + ts

--- client/src/components/UI/IconBox/IconBox.tsx
+++ client/src/components/UI/IconBox/IconBox.tsx
@@ -5,7 +5,11 @@ import './IconBox.scss'
 
 const cn = withNaming({ n: '', e: '__', m: '_' })
 
-const IconBox = ({ children, className, textStyle }) => {
+interface IconBoxProps {
+  textStyle: {}
+}
+
+const IconBox: React.FC<IconBoxProps> = ({ children, textStyle }) => {
   const cnIconBox = cn('icon-box')
   const cnText = cn('text')
 
@@ -14,15 +18,6 @@ const IconBox = ({ children, className, textStyle }) => {
       <div className={`${cnIconBox('icon')} ${cnText(textStyle)}`}>{children}</div>
     </div>
   )
-}
-
-IconBox.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.objectOf(PropTypes.string || PropTypes.bool),
-}
-IconBox.defaultProps = {
-  children: '',
-  className: {},
 }
 
 export default IconBox
